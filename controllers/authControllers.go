0ce600591b6df6766ab7b8b23b6b3d8251d9fd86
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-01-06 23:20:20 -0500

    refactored responses on authController

--- controllers/authControllers.go
+++ controllers/authControllers.go
@@ -12,7 +12,7 @@ var CreateAccount = func(w http.ResponseWriter, r *http.Request) {
 	account := &models.Account{}
 	err := json.NewDecoder(r.Body).Decode(account) //decode the request body into struct and failed if any error occur
 	if err != nil {
-		u.Respond(w, u.Message(false, "Invalid request"))
+		http.Error(w, "Invalid Request", http.StatusBadRequest)
 		return
 	}
 
@@ -25,7 +25,7 @@ var Authenticate = func(w http.ResponseWriter, r *http.Request) {
 	account := &models.Account{}
 	err := json.NewDecoder(r.Body).Decode(account) //decode the request body into struct and failed if any error occur
 	if err != nil {
-		u.Respond(w, u.Message(false, "Invalid request"))
+		http.Error(w, "Invalid Request", http.StatusBadRequest)
 		return
 	}
 
